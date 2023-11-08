a691d417313d480d6431acf049afa655bd559455
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2023-11-08 14:22:38 -0800

    Added icon buttons and logo to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,43 @@
 import "./App.css";
+import logo from '../src/assets/images/logo.jpg'
+import MenuContainer from "./components/MenuContainer";
+import { ControlPoint, Favorite, Login, Person } from '@mui/icons-material';
+
+
 
 function App() {
   return (
     <div className="App">
 
-      <div className="Menu">
-        <h2>Menu</h2>
-        <img src="logo.jpg" alt="PinThis logo" className="Logo"/>
+      <div className="menuContainer">
+  
+        <img src={logo} alt="PinThis logo" className="Logo"/>
+
+      <h2>Menu</h2>
+          <div className="Navbar">
+            <div>
+              <div className="iconContainer">
+                <ControlPoint />
+                <span>Add Post</span>
+              </div>
+              <div className="iconContainer">
+                <Favorite />
+                <span>Favorites</span>
+              </div>
+
+              <div className="iconContainer">
+                <Login />
+                <span>Login</span>
+              </div>
+              <div className="iconContainer">
+                <Person />
+                <span>Profile</span>
+              </div>
+            </div>
+          </div>
       </div>
 
-      <div className="Navbar">
 
-      </div>
 
       <main></main>
     </div>
