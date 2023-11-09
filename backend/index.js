8802beed4834553ae3388e4b5c845e5bfd1cdd29
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-11-08 21:23:06 -0800

    starting controllers over

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
-const usersRouter = require('./controllers/users')
 
 const app = express()
 
 // Controllers & Routes
-app.use('/users', usersRouter)
+app.use('/users', require('./controllers/users'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
