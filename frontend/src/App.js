510b0b4432768c93d9f1ac77816a5791cdb05304
Jason <dodgerblue23@icloud.com>
2023-11-12 08:19:18 -0800

    pages, login and signup forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import './App.css'; 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from './components/nav';
-import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <Nav />
     </div>
   );
 }
---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import PinThislogo from './images/PinThis-logo.jpeg';
+
+export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profilePicture, setProfilePicture] = useState(null);
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleProfilePictureChange = (e) => setProfilePicture(e.target.files[0]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signup-page">
+      <div className="signup-title">
+        <h1>Create an Account</h1>
+      </div>
+      <div className="signup-container">
+        <div className="signup-form">
+          <img className="signup-logo" src={PinThislogo} alt="PinThis Logo" onClick={() => window.location.href = '/'} />
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmailChange} />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPicture">
+              <Form.Label>Profile Picture</Form.Label>
+              <Form.Control type="file" accept="image/*" onChange={handleProfilePictureChange} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Signup
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
