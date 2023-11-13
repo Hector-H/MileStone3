5af5d1281a7c0cc1ead3f3483bc5c09d74e18c5a
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2023-11-13 14:19:13 -0800

    AddPin page is styled

--- frontend/src/components/AddPin.js
+++ frontend/src/components/AddPin.js
@@ -1,42 +1,51 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import '../css/AddPin.css';
+import board3 from '../assets/images/board3.jpg';
 
 const AddPin = ({ onAddPin, setShowAddPin }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [photo, setPhoto] = useState(null);
 
   const handleAddPin = (e) => {
     e.preventDefault();
-    onAddPin({ title, description });
+    onAddPin({ title, description, photo });
     setTitle('');
     setDescription('');
+    setPhoto(null);
     setShowAddPin(false);
   };
 
   return (
-<div className="add-pin-page">
-  <div className="add-pin-title">
-    <h1>New Pin</h1>
-  </div>
-  <div className="add-pin-container">
-    <Form onSubmit={handleAddPin}>
-      <Form.Group className="mb-3" controlId="formPinTitle">
-        <Form.Label>Title:</Form.Label>
-        <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formPinDescription">
-        <Form.Label>Description:</Form.Label>
-        <Form.Control as="textarea" value={description} onChange={(e) => setDescription(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Add Pin
-      </Button>
-    </Form>
-  </div>
-</div>
-
-    );
+    <div className="add-pin-page">
+      <div className="add-pin-title">
+        <h1>Add A New Pin</h1>
+      </div>
+      <div className="add-pin-container">
+        <img src={board3} alt="Blue mood board" />
+        <div className="add-pin-form">
+          <Form onSubmit={handleAddPin}>
+            <Form.Group className="mb-3" controlId="formPinTitle">
+              <Form.Label>Title:</Form.Label>
+              <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formPinDescription">
+              <Form.Label>Description: </Form.Label>
+              <Form.Control as="textarea" value={description} onChange={(e) => setDescription(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPinPhoto" className="mb-3">
+              <Form.Label>Upload Photo: </Form.Label>
+              <Form.Control type="file" accept="image/*" onChange={(e) => setPhoto(e.target.files[0])} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add Pin
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPin;
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -28,7 +28,7 @@ export default function Signup() {
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address: </Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmailChange} />
+              <Form.Control type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
