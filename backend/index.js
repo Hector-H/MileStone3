8c634f942b5fc75e678c7062fa066abd7e25428b
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-11-16 22:40:54 -0600

    added middleware

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,27 @@
 // Modules and Globals
-require('dotenv').config();
-const express = require('express');
-const userRouter = require('./controllers/users');
-const pinRouter = require('./controllers/pins');
-const postRouter = require('./controllers/posts');
+require('dotenv').config()
+const express = require('express')
+const userRouter = require('./controllers/users')
+const pinRouter = require('./controllers/pins')
+const postRouter = require('./controllers/posts')
+const authenticate = require('./middleware/auth')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+
+// Middleware
+app.use(authenticate)
+
 
 // Controllers & Routes
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
+app.use('/users', userRouter)
+app.use('/posts', postRouter)
 /* app.use('/pins', pinRouter); */
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(err.stack)
+    res.status(500).json({ error: 'Internal Server Error' })
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT)
